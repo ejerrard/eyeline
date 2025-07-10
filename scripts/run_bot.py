e22f6ab4bc242d3f775c6634f253a1684914c934
ejerrard <65082047+ejerrard@users.noreply.github.com>
2025-07-10 12:10:15 +1000

    chore: replace broken rss link

--- scripts/run_bot.py
+++ scripts/run_bot.py
@@ -2,7 +2,7 @@ from app.feeds import fetch_items
 
 
 def main():
-    rss_url = "https://parlinfo.aph.gov.au/parlInfo/feeds/rss.w3p;adv=yes;orderBy=date-eFirst;page=0;query=Date%3AthisYear%20Dataset%3Abillsdgs;resCount=100"  # Official APH bills feed
+    rss_url = "https://www.aph.gov.au/senate/rss/new_inquiries"
     items = fetch_items(rss_url)
 
     for item in items:
